ae9d2f5d3c4ba4ab3a34887b65171c0a221b492b
Sergio Santoyo <sergiosantoyo.92@gmail.com>
2018-01-31 11:07:58 -0600

    fixed acc

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -3,8 +3,8 @@
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-static VectorXd _cartesian_to_polar(const VectorXd &x_state);
-static VectorXd _innovate_polar(const VectorXd &z, const VectorXd &z_pred);
+static VectorXd cartesian_polar(const VectorXd &x_state);
+static VectorXd norm_polar(const VectorXd &z, const VectorXd &z_pred);
 
 KalmanFilter::KalmanFilter() {}
 
@@ -55,27 +55,26 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
-  // convert radar measurements from cartesian coordinates (x, y, vx, vy) to polar (rho, phi, rho_dot).
-  VectorXd z_pred = _cartesian_to_polar(x_);
-  VectorXd y = _innovate_polar(z, z_pred);
 
-  // following is exact the same as in the function of KalmanFilter::Update()
+  VectorXd z_pred = cartesian_polar(x_);
+  VectorXd y = norm_polar(z, z_pred);
+
   MatrixXd Ht = H_.transpose();
   MatrixXd PHt = P_ * Ht;
   MatrixXd S = H_ * PHt + R_;
   MatrixXd Si = S.inverse();
   MatrixXd K = PHt * Si;
 
-  //new estimation
+  //new estimate
   x_ = x_ + (K * y);
   long x_size = x_.size();
   MatrixXd I = MatrixXd::Identity(x_size, x_size);
   P_ = (I - K * H_) * P_;
 }
 
-static VectorXd _cartesian_to_polar(const VectorXd &x_state)
+static VectorXd cartesian_polar(const VectorXd &x_state)
 {
-	// (px, py, vx, vy) => (rho, phi, rho_dot)
+
 	const float eps	= 0.000001;
 	float px, py, vx, vy;
 	px = x_state[0];
@@ -98,11 +97,11 @@ static VectorXd _cartesian_to_polar(const VectorXd &x_state)
 	return z_pred;
 }
 
-static VectorXd _innovate_polar(const VectorXd &z, const VectorXd &z_pred)
+static VectorXd norm_polar(const VectorXd &z, const VectorXd &z_pred)
 {
 	  VectorXd y = z - z_pred;
 
-	  // normalize the angle between -pi to pi
+	  // normalize the angle
 	  while(y(1) > M_PI){
 	    y(1) -= M_PI*2;
 	  }
